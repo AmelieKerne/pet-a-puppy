3217b09bbb9ff1a7a4291948d4a1640ee32b73a6
Lena Adam <lena.adam@me.com>
2020-08-18 13:06:53 +0200

    add views to devise and basic bootstrap for sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_08_17_150524) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
