af8a86f17193860a1be6c12ecfd3ccd3f46e4be5
Lena Adam <lena.adam@me.com>
2020-08-18 17:00:22 +0200

    add new and create actions for booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+  def new
+    @puppy = Puppy.find(params[:puppy_id])
+    @booking = Booking.new
+    @puppies = Puppy.all
+    authorize @booking
+  end
+
+  def create
+    @puppy = Puppy.find(params[:puppy_id])
+    @booking = Booking.new(booking_params)
+    authorize @booking
+    @booking.puppy = @puppy
+    if @booking.save
+      redirect_to puppy_path(@puppy)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :puppy_id)
+  end
+
 end
