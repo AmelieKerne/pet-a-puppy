f436e670461d79760724574979d3adf0878d9574
Amelie Kerne <amelie.kerne@gmail.com>
2020-08-18 13:11:52 +0200

    puppy controller + puppy policy + puppy views files created

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,17 +1,31 @@
 class PuppiesController < ApplicationController
+
+  def index
+    @puppies = Puppy.all
+    authorize @puppy
+  end
+
+  def show
+    @puppy = Puppy.find(params[:id])
+    authorize @puppy
+  end
+
   def new
     @puppy = Puppy.new
   end
 
   def create
+    @puppy = Puppy.new(puppy_params)
+    if @puppy.save
+      redirect_to puppy_path(@puppy)
+    else
+      render 'new'
   end
 
-  def index
-    @puppies = Puppy.all
-  end
+  private
 
-  def show
-    @puppy = Puppy.find(params[:id])
+  def puppy_params
+    params.require(:puppy).permit(:name, :description, :price)
   end
 
 end
