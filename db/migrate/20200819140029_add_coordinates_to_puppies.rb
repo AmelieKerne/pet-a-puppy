469939251e982fc6b15d43f6e1e05e9b582fb35b
Mohit Rao <mohitrao.87@gmail.com>
2020-08-19 16:22:01 +0200

    brunos orders

--- db/migrate/20200819140029_add_coordinates_to_puppies.rb
+++
@@ -1,7 +0,0 @@
-class AddCoordinatesToPuppies < ActiveRecord::Migration[6.0]
-  def change
-    add_column :puppies, :latitude, :float
-    add_column :puppies, :longitude, :float
-    add_column :puppies, :address, :string
-  end
-end
