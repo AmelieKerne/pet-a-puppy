83cdaa8864bb55862ef038507243ae111ab02985
Amelie Kerne <amelie.kerne@gmail.com>
2020-08-20 13:52:27 +0200

    basic seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+puts "creating users"
+Booking.destroy_all
+Puppy.destroy_all
+User.destroy_all
+2.times do
+  user = User.new(
+    #name: Faker::FunnyName.name,
+    email: Faker::Internet.email,
+    password: "123456",
+  )
+  user.save!
+end
+puts "users created"
+
+puts "creating puppies"
+
+10.times do
+  file = URI.open('https://alop.org/wp-content/uploads/2014/12/happy-dog.jpg')
+   puppy = Puppy.new(
+    name: Faker::Superhero.name,
+    description: Faker::ChuckNorris.fact,
+    price: Faker::Commerce.price.to_i,
+    user: User.first,
+    address: "Berlin",
+  )
+  puppy.photo.attach(io: file, filename: "puppy_profile_picture.jpg", content_type: 'image/jpg')
+  puppy.save!
+end
+
+puts "puppies created"
